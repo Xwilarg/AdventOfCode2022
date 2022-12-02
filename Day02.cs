7cc3dc247be430a0fd2768179dd4e4cc21134807
Xwilarg <xwilarg@protonmail.com>
2022-12-02 10:02:35 +0100

    Add day 2 part 2

--- Day02.cs
+++ Day02.cs
@@ -19,7 +19,17 @@
 
         public string Part2(string input)
         {
-            return string.Empty;
+            return input.Split('\n').Select(x =>
+            {
+                var a1 = 3 - ('C' - x[0]);
+                var a2 = 3 - ('Z' - x[2]);
+                var d = a2 - a1;
+
+                return ((a2 - 1) * 3) +
+                (a2 == 2 ? a1
+                    : a2 == 1 ? (a1 == 1 ? 3 : a1 - 1)
+                    : (a1 == 3 ? 1 : a1 + 1));
+            }).Sum().ToString();
         }
     }
 }
