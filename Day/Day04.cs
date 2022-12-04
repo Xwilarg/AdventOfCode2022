dea39e82a65d3f187d1ec7093acaba486334d5da
Xwilarg <xwilarg@protonmail.com>
2022-12-04 15:31:04 +0100

    Add day 4 part 2

--- Day/Day04.cs
+++ Day/Day04.cs
@@ -2,23 +2,34 @@
 {
     public class Day04 : IDay
     {
+        private static int[][] GetRanges(string data)
+        {
+            var arr = data.Split(",").ToArray();
+
+            // Ranges values are separated by a -
+            return arr.Select(y => y.Split('-').Select(z => int.Parse(z)).ToArray()).ToArray();
+        }
+
         public int Part1(string input)
         {
             return input.Split('\n').Count(x =>
             {
-                var data = x.Split(",").ToArray();
+                var rd = GetRanges(x);
 
-                // Ranges
-                var rd = data.Select(y => y.Split('-').Select(z => int.Parse(z)).ToArray()).ToArray();
-
-                // Does they contain each other?
+                // Does they englobe each other?
                 return (rd[0][0] >= rd[1][0] && rd[0][1] <= rd[1][1]) || (rd[1][0] >= rd[0][0] && rd[1][1] <= rd[0][1]);
             });
         }
 
         public int Part2(string input)
         {
-            return 0;
+            return input.Split('\n').Count(x =>
+            {
+                var rd = GetRanges(x);
+
+                // The minimum value of one of the range is inside the other
+                return (rd[0][0] >= rd[1][0] && rd[0][0] <= rd[1][1]) || (rd[1][0] >= rd[0][0] && rd[1][0] <= rd[0][1]);
+            });
         }
     }
 }
