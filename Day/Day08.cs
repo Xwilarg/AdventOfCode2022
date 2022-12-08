b69ffc3a70b30ca94acc4b82f870406375cee9c5
Xwilarg <xwilarg@protonmail.com>
2022-12-08 09:29:05 +0100

    Add day 8 part 2

--- Day/Day08.cs
+++ Day/Day08.cs
@@ -8,12 +8,24 @@
             public required int Y { init; get; }
         }
 
-        private static bool IsLineOkay(int x, int y, int value, Vector2Int dir, int[][] map)
+        // All directions we can do
+        private Vector2Int[] directions = new Vector2Int[]
         {
+            new() { X = 1, Y = 0 },
+            new() { X = -1, Y = 0 },
+            new() { X = 0, Y = 1 },
+            new() { X = 0, Y = -1 }
+        };
+
+        private static bool IsLineOkay(int x, int y, int value, Vector2Int dir, int[][] map, out int itCount)
+        {
+            itCount = 0;
+
             var dx = dir.X;
             var dy = dir.Y;
             do
             {
+                itCount++;
                 if (map[y + dy][x + dx] >= value)
                 {
                     // Found a tree bigger, the line isn't good
@@ -28,19 +40,13 @@
             return true;
         }
 
+        // Convert the input to a int[][] that represent the map
+        private static int[][] ParseInput(string input)
+            => input.Split('\n').Select(y => y.ToCharArray().Select(x => x - '0').ToArray()).ToArray();
+
         public string Part1(string input)
         {
-            // Convert the input to a int[][] that represent the map
-            var map = input.Split('\n').Select(y => y.ToCharArray().Select(x => x - '0').ToArray()).ToArray();
-
-            // All directions we can do
-            var directions = new[]
-            {
-                new Vector2Int() { X = 1, Y = 0 },
-                new Vector2Int() { X = -1, Y = 0 },
-                new Vector2Int() { X = 0, Y = 1 },
-                new Vector2Int() { X = 0, Y = -1 }
-            };
+            var map = ParseInput(input);
 
             var visibleTreesCount = (map.Length * 2) + (map[0].Length * 2) - 4; // Trees on the border
 
@@ -51,7 +57,7 @@
                     foreach (var dir in directions)
                     {
                         // If a direction is valid we add the tree as a visible one and go to the next one
-                        if (IsLineOkay(x, y, map[y][x], dir, map))
+                        if (IsLineOkay(x, y, map[y][x], dir, map, out _))
                         {
                             visibleTreesCount++;
                             break;
@@ -65,7 +71,28 @@
 
         public string Part2(string input)
         {
-            return string.Empty;
+            var map = ParseInput(input);
+
+            var bestScore = 0;
+
+            for (var y = 1; y < map.Length - 1; y++)
+            {
+                for (var x = 1; x < map[y].Length - 1; x++)
+                {
+                    var score = 1;
+                    foreach (var dir in directions)
+                    {
+                        IsLineOkay(x, y, map[y][x], dir, map, out int mult);
+                        score *= mult;
+                    }
+                    if (score > bestScore)
+                    {
+                        bestScore = score;
+                    }
+                }
+            }
+
+            return bestScore.ToString();
         }
     }
 }
