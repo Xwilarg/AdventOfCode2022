2f996a206de1944c2014122d59a7145651fd9723
Xwilarg <xwilarg@protonmail.com>
2022-12-10 12:39:30 +0100

    Add day 10 part 1

---
+++ Day/Day10.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCode2022.Day
+{
+    public class Day10 : IDay
+    {
+        public string Part1(string input)
+        {
+            var cycle = 1;
+            var value = 1;
+            var score = 0;
+
+            void increaseCycle()
+            {
+                cycle++;
+
+                if ((cycle - 20) % 40 == 0)
+                {
+                    score += value * cycle;
+                }
+            }
+
+            foreach (var line in input.Split('\n'))
+            {
+                var data = line.Split();
+
+                if (data[0] != "noop")
+                {
+                    increaseCycle();
+                    value += int.Parse(data[1]);
+                }
+                increaseCycle();
+            }
+
+            return score.ToString();
+        }
+
+        public string Part2(string input)
+        {
+            return string.Empty;
+        }
+    }
+}
